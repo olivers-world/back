ebb57e0e78339b643a965a595ca6cdd1ce823883
Synaptique <garibaldialexis@gmail.com>
2024-03-10 21:06:10 +0100

    Ajout systeme JWT

--- src/api/controllers/authController.js
+++ src/api/controllers/authController.js
@@ -1,5 +1,9 @@
-const db = require('../../config/db.js');
+require("dotenv").config();
+const db = require("../../config/db.js");
 const bcrypt = require("bcrypt");
+
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.JWT_TOKEN;
 
 // Inscription
 exports.register = (req, res) => {
@@ -35,16 +39,49 @@ exports.register = (req, res) => {
 
       // Insert new user into database
       const insertUserQuery =
-        "INSERT INTO Utilisateurs (Nom, Prenom, Email, MotDePasse) VALUES (?, ?, ?, ?)";
-      db.query(insertUserQuery, [prenom, nom, email, hashedPwd], (err, result) => {
-        if (err) throw err;
-        res.status(201).json({ message: "User registered successfully" });
-      });
+        "INSERT INTO Utilisateurs (Nom, Prenom, Email, MotDePasse, Role) VALUES (?, ?, ?, ?, 'Utilisateur')";
+      db.query(
+        insertUserQuery,
+        [prenom, nom, email, hashedPwd],
+        (err, result) => {
+          if (err) throw err;
+          res.status(201).json({ message: "User registered successfully" });
+        }
+      );
     }
   });
 };
 
 // Connexion
 exports.login = (req, res) => {
-  // Implémentation de la connexion
+  const { email, pwd } = req.body;
+
+  if (!(email && pwd)) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+
+  const findUserQuery = "SELECT * FROM Utilisateurs WHERE Email = ?";
+  db.query(findUserQuery, [email], async (err, result) => {
+    if (err) throw err;
+
+    if (result.length > 0) {
+      const user = result[0];
+
+      const isMatch = await bcrypt.compare(pwd, user.MotDePasse);
+      if (isMatch) {
+        // Ici, vous générez le token d'accès. Vous pouvez aussi inclure d'autres données utilisateur selon les besoins.
+        const accessToken = jwt.sign(
+          { userId: user.Id, email: user.Email, roles: user.Role }, // Assurez-vous que les rôles sont stockés dans votre DB et récupérés correctement
+          secretKey,
+          { expiresIn: '1h' } // Le token expire après 1 heure
+        );
+
+        res.status(200).json({ accessToken, roles: user.Role });
+      } else {
+        res.status(401).json({ message: "Invalid email or password" });
+      }
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  });
 };
