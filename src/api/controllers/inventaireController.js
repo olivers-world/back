c7fbb748f1d557e4a2afcbd3451dd484412524dc
Synaptique <garibaldialexis@gmail.com>
2024-04-01 06:22:54 +0200

    [FEAT] Dashboard

--- src/api/controllers/inventaireController.js
+++ src/api/controllers/inventaireController.js
@@ -1,80 +1,55 @@
 require("dotenv").config();
 const db = require("../../config/db.js");
+const dayjs = require('dayjs')
 
 exports.createInventaire = (req, res) => {
   // Ici, vous récupérez les données envoyées avec la requête POST.
-  const { email, nom, date, nbPersonne } = req.body;
+  const { date } = req.body;
 
   // Valider les données reçues
-  if (!(email && nom && date && nbPersonne)) {
-    return res
-      .status(400)
-      .json({ message: "Email, nom, date, and number of people are required" });
+  if (!date) {
+    return res.status(400).json({ message: "Date is required" });
   }
 
-  // Vérifier s'il y a assez de place pour la nouvelle réservation
-  const checkAvailabilityQuery = `
-    SELECT SUM(NbPersonnes) as TotalPersonnes
-    FROM Reservations
-    WHERE DateHeure BETWEEN ? AND DATE_ADD(?, INTERVAL 1 DAY)
-    AND (Statut = 'Prise' OR Statut = 'En cours')
-  `;
+  const createReservationQuery = `
+        INSERT INTO Inventaires (Date, Fait)
+        VALUES (?, 0)
+      `;
 
-  db.query(checkAvailabilityQuery, [date, date], (err, result) => {
+  // Exécution de la requête SQL pour créer la réservation
+  db.query(createReservationQuery, [date], (err, result) => {
     if (err) {
       // Gérer les erreurs de la base de données ici
       return res.status(500).json({ message: "Database error", error: err });
-    }
-
-    const totalReserved = result[0].TotalPersonnes || 0;
-
-    if (parseInt(totalReserved, 10) + parseInt(nbPersonne, 10) > maxPeople) {
-      // Si on dépasse la capacité max, on refuse la réservation
-      return res.status(400).json({
-        message: "Pas assez de place",
-      });
     } else {
-      // Il y a assez de place, on peut insérer la nouvelle réservation
-      const createReservationQuery = `
-        INSERT INTO Reservations (Email, Nom, DateHeure, NbPersonnes, Statut)
-        VALUES (?, ?, ?, ?, 'Prise')
-      `;
-
-      // Exécution de la requête SQL pour créer la réservation
-      db.query(
-        createReservationQuery,
-        [email, nom, date, nbPersonne],
-        (err, result) => {
-          if (err) {
-            // Gérer les erreurs de la base de données ici
-            return res
-              .status(500)
-              .json({ message: "Database error", error: err });
-          } else {
-            // Réussite de l'insertion
-            return res.status(201).json({
-              message: "Reservation creer avec succes",
-              reservationId: result.insertId,
-            });
-          }
-        }
-      );
+      // Réussite de l'insertion
+      return res.status(201).json({
+        message: "Inventaire creer avec succes",
+        reservationId: result.insertId,
+      });
     }
   });
 };
 
 exports.getMostRecentInventaire = (req, res) => {
-  const getInventairesQuery = `SELECT * FROM Inventaire ORDER BY Date DESC LIMIT 1`;
+  const formattedToday = dayjs().format("YYYY-MM-DD");
+
+  const getInventairesQuery = `
+  SELECT *, ABS(DATEDIFF(Date, '${formattedToday}')) AS DaysDifference
+  FROM Inventaires
+  ORDER BY DaysDifference ASC, Date DESC
+  LIMIT 1`;
 
   db.query(getInventairesQuery, (err, results) => {
     if (err) {
       return res.status(500).json({ message: "Database error", error: err });
+    } else if (results.length === 0) {
+      return res.status(404).json({ message: "No inventaire found" });
     } else {
       return res.status(200).json(results[0]);
     }
   });
 };
-
 
 exports.updateInventaire = (req, res) => {
   // Récupération de l'ID de la réservation à modifier ainsi que des nouvelles valeurs
@@ -86,7 +61,7 @@ exports.updateInventaire = (req, res) => {
   }
 
   // Construction de la requête SQL de mise à jour
-  let updateQuery = `UPDATE Inventaire SET `;
+  let updateQuery = `UPDATE Inventaires SET `;
   const queryParams = [];
 
   if (newDate) {
@@ -124,7 +99,7 @@ exports.deleteInventaire = (req, res) => {
     return res.status(400).json({ message: "Inventaire ID is required" });
   }
 
-  const deleteQuery = `DELETE FROM Inventaire WHERE ID = ?`;  
+  const deleteQuery = `DELETE FROM Inventaires WHERE ID = ?`;
 
   db.query(deleteQuery, [id], (err, result) => {
     if (err) {
--- src/api/controllers/nettoyageController.js
+++ src/api/controllers/nettoyageController.js
@@ -1,80 +1,50 @@
 require("dotenv").config();
 const db = require("../../config/db.js");
+const dayjs = require('dayjs')
 
 exports.createNettoyage = (req, res) => {
-  // Ici, vous récupérez les données envoyées avec la requête POST.
-  const { email, nom, date, nbPersonne } = req.body;
+  const { date } = req.body;
 
-  // Valider les données reçues
-  if (!(email && nom && date && nbPersonne)) {
-    return res
-      .status(400)
-      .json({ message: "Email, nom, date, and number of people are required" });
+  if (!date) {
+    return res.status(400).json({ message: "Date is required" });
   }
 
-  // Vérifier s'il y a assez de place pour la nouvelle réservation
-  const checkAvailabilityQuery = `
-    SELECT SUM(NbPersonnes) as TotalPersonnes
-    FROM Nettoyage
-    WHERE DateHeure BETWEEN ? AND DATE_ADD(?, INTERVAL 1 DAY)
-    AND (Statut = 'Prise' OR Statut = 'En cours')
-  `;
+  const createReservationQuery = `INSERT INTO Nettoyages (Date, Fait) VALUES (?, 0)`;
 
-  db.query(checkAvailabilityQuery, [date, date], (err, result) => {
-    if (err) {
-      // Gérer les erreurs de la base de données ici
-      return res.status(500).json({ message: "Database error", error: err });
+  db.query(
+    createReservationQuery,
+    [date],
+    (err, result) => {
+      if (err) {
+        // Gérer les erreurs de la base de données ici
+        return res.status(500).json({ message: "Database error", error: err });
+      } else {
+        // Réussite de l'insertion
+        return res.status(201).json({ message: "Nettoyage creer avec succes" });
+      }
     }
-
-    const totalReserved = result[0].TotalPersonnes || 0;
-
-    if (parseInt(totalReserved, 10) + parseInt(nbPersonne, 10) > maxPeople) {
-      // Si on dépasse la capacité max, on refuse la réservation
-      return res.status(400).json({
-        message: "Pas assez de place",
-      });
-    } else {
-      // Il y a assez de place, on peut insérer la nouvelle réservation
-      const createReservationQuery = `
-        INSERT INTO Reservations (Email, Nom, DateHeure, NbPersonnes, Statut)
-        VALUES (?, ?, ?, ?, 'Prise')
-      `;
-
-      // Exécution de la requête SQL pour créer la réservation
-      db.query(
-        createReservationQuery,
-        [email, nom, date, nbPersonne],
-        (err, result) => {
-          if (err) {
-            // Gérer les erreurs de la base de données ici
-            return res
-              .status(500)
-              .json({ message: "Database error", error: err });
-          } else {
-            // Réussite de l'insertion
-            return res.status(201).json({
-              message: "Reservation creer avec succes",
-              reservationId: result.insertId,
-            });
-          }
-        }
-      );
-    }
-  });
+  );
 };
 
 exports.getMostRecentNettoyage = (req, res) => {
-  const getNettoyageQuery = `SELECT * FROM Nettoyage ORDER BY Date DESC LIMIT 1`;
+  const formattedToday = dayjs().format("YYYY-MM-DD");
+
+  const getNettoyageQuery = `
+  SELECT *, ABS(DATEDIFF(Date, '${formattedToday}')) AS DaysDifference
+  FROM Nettoyages
+  ORDER BY DaysDifference ASC, Date DESC
+  LIMIT 1`;
 
   db.query(getNettoyageQuery, (err, results) => {
     if (err) {
       return res.status(500).json({ message: "Database error", error: err });
+    } else if (results.length === 0) {
+      return res.status(404).json({ message: "No inventaire found" });
     } else {
       return res.status(200).json(results[0]);
     }
   });
 };
-
 
 exports.updateNettoyage = (req, res) => {
   // Récupération de l'ID de la réservation à modifier ainsi que des nouvelles valeurs
@@ -86,7 +56,7 @@ exports.updateNettoyage = (req, res) => {
   }
 
   // Construction de la requête SQL de mise à jour
-  let updateQuery = `UPDATE Nettoyage SET `;
+  let updateQuery = `UPDATE Nettoyages SET `;
   const queryParams = [];
 
   if (newDate) {
@@ -124,7 +94,7 @@ exports.deleteNettoyage = (req, res) => {
     return res.status(400).json({ message: "Nettoyage ID is required" });
   }
 
-  const deleteQuery = `DELETE FROM Nettoyage WHERE ID = ?`;  
+  const deleteQuery = `DELETE FROM Nettoyages WHERE ID = ?`;
 
   db.query(deleteQuery, [id], (err, result) => {
     if (err) {
